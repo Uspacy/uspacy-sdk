7e51b2368472501d150f1a72046d996ea3f7c1b3
Vadym <vadymsenior@gmail.com>
2022-10-10 21:28:22 +0300

    add all methods from sdk

---
+++ src/services/FilesService/FilesService.ts
@@ -0,0 +1,116 @@
+import { AxiosInstance } from "axios";
+
+/**
+ * Tasks service
+ */
+export class FilesService {
+	private namespace = "files/v1/files";
+
+	private routeAceessName = {
+		BaseURL: `${this.namespace}`,
+		getListTheFileInfoURL: (
+			entityType: EntityType[],
+			entityId: number
+		): string =>
+			`${this.routeAceessName}/files?entity_type=${entityType}&entityId=${entityId}/`,
+		showInfoAboutFileURL: (id: number): string =>
+			`${this.routeAceessName.BaseURL}/${id}`,
+		uploadOneOrMoreFilesURL: (): string => `${this.namespace}/`,
+	};
+
+	/**
+	 * @param config http client
+	 */
+	constructor(private httpClient: AxiosInstance) {}
+
+	/**
+	 * Lists the file info
+	 * @param entityType Entity type name
+	 * @param entityId Entity ID of the file
+	 * @returns
+	 */
+	getListTheFileInfo(entityType: EntityType[], entityId: number) {
+		return this.httpClient.get<FileInfoDto>(
+			`${this.routeAceessName.getListTheFileInfoURL(
+				entityType,
+				entityId
+			)}`
+		);
+	}
+
+	/**
+	 * Upload one or more files
+	 * @param files files
+	 * @param entityType Entity type name
+	 * @param entityId Entity ID of the file
+	 * @returns
+	 */
+	uploadOneOrMoreFiles(
+		files: string[],
+		entityType: EntityType,
+		entityId: number
+	) {
+		return this.httpClient.post<FileInfoDto>(
+			`${this.routeAceessName.uploadOneOrMoreFilesURL()}`,
+			{
+				files,
+				entityType,
+				entityId,
+			}
+		);
+	}
+
+	/**
+	 * Shows the info about the file
+	 * @param fileId fileid
+	 * @returns
+	 */
+	showInfoAboutFile(fileId: number) {
+		return this.httpClient.get<FileInfoDto>(
+			`${this.routeAceessName.showInfoAboutFileURL(fileId)}`
+		);
+	}
+
+	/**
+	 * Update the entity_id for one file or a group of files
+	 * @param fileId Numeric ID of the file to update or the UUID of the upload group
+	 * @param entity_id Entity ID of the file
+	 * @returns
+	 */
+	updateEntityIdForFiles(fileId: number, entity_id: number) {
+		return this.httpClient.patch<FileInfoDto>(
+			`${this.routeAceessName.showInfoAboutFileURL(fileId)}`,
+			{
+				entity_id,
+			}
+		);
+	}
+
+	/**
+	 * Removes the uploaded file and info about it
+	 * @param fileId file id
+	 * @returns
+	 */
+	removeFileAndInfo(fileId: number) {
+		return this.httpClient.delete<FileInfoDto>(
+			`${this.routeAceessName.showInfoAboutFileURL(fileId)}`
+		);
+	}
+}
+
+export enum EntityType {
+	comment = "comment",
+	post = "post",
+	task = "task",
+}
+
+export interface FileInfoDto {
+	id: number;
+	entityType: EntityType[];
+	entityId: number;
+	uploadId: string;
+	originalFilename: string;
+	lastModified: number;
+	size: number;
+	url: string;
+}
--- src/services/Users/Users.ts
+++ src/services/Users/Users.ts
@@ -1,10 +1,44 @@
-import { AxiosInstance } from 'axios';
+import { AxiosInstance } from "axios";
+import { type } from "os";
 
 /**
  * Users service
  */
 export class Users {
-	private namespace = '/company/v1';
+	private namespace = "/company/v1";
+
+	private routeAceessName = {
+		usersServiceBaseURL: `${this.namespace}/users`,
+		getUsersURL: (): string =>
+			`${this.routeAceessName.usersServiceBaseURL}`,
+		userByIDURL: (userId: number): string =>
+			`${this.routeAceessName.usersServiceBaseURL}/${userId}`,
+		userDeactivateURL: (userId: number): string =>
+			`${this.routeAceessName.userByIDURL(userId)}/deactivate/`,
+		userActivateURL: (userId: number): string =>
+			`${this.routeAceessName.userByIDURL(userId)}/activate/`,
+		userUpdateRolesURL: (userId: number): string =>
+			`${this.routeAceessName.userByIDURL(userId)}/updateRoles/`,
+		userUpdatePositionURL: (userId: number): string =>
+			`${this.routeAceessName.userByIDURL(userId)}/updatePosition/`,
+		userUpdatePasswordURL: (userId: number): string =>
+			`${this.routeAceessName.userByIDURL(userId)}/changePassword/`,
+		getSelfProfileURL: (): string =>
+			`${this.routeAceessName.getUsersURL()}/me/`,
+		updateSelfPasswordURL: (): string =>
+			`${this.routeAceessName.getSelfProfileURL()}changePassword/`,
+		forgotPasswordURL: (): string =>
+			`${this.routeAceessName.getUsersURL()}/forgotPassword/`,
+		resetPasswordURL: (): string =>
+			`${this.routeAceessName.getUsersURL()}/resetPassword/`,
+		searchURL: (
+			email: string,
+			firstName: string,
+			lastName: string
+		): string =>
+			`${this.routeAceessName.getUsersURL()}/search/?email=${email}&firstName=${firstName}&LastName=${lastName}`,
+	};
+
 	/**
 	 * @param config http client
 	 */
@@ -17,12 +51,15 @@ export class Users {
 	 * @returns Array users entity
 	 */
 	list(page: number, list: number) {
-		return this.httpClient.get<User[]>(`${this.namespace}/users/`, {
-			params: {
-				page,
-				list,
-			},
-		});
+		return this.httpClient.get<User[]>(
+			`${this.routeAceessName.getUsersURL()}`,
+			{
+				params: {
+					page,
+					list,
+				},
+			}
+		);
 	}
 
 	/**
@@ -31,14 +68,195 @@ export class Users {
 	 * @returns user entity
 	 */
 	getById(id: number) {
-		return this.httpClient.get<User>(`${this.namespace}/users/${id}/`);
+		return this.httpClient.get<User>(
+			`${this.routeAceessName.userByIDURL(id)}`
+		);
+	}
+
+	/**
+	 * Sign up user by firstName, lastName, email, password.
+	 * @param email user email
+	 * @param password user password
+	 * @param firstName	user firstName
+	 * @param lastName user lastName
+	 * @returns created user
+	 */
+	createUser(
+		email: string,
+		password: string,
+		firstName: string,
+		lastName: string
+	) {
+		return this.httpClient.post<User>(
+			`${this.routeAceessName.getUsersURL()}`,
+			{
+				email,
+				password,
+				firstName,
+				lastName,
+			}
+		);
+	}
+
+	/**
+	 * Update user data by id.
+	 * @param id user id
+	 * @param config user info (interface)
+	 * @returns updated user
+	 */
+
+	updateUser(id: number, config: updateUser) {
+		return this.httpClient.patch<User>(
+			`${this.routeAceessName.userByIDURL(id)}`,
+			config
+		);
+	}
+
+	/**
+	 * Deactivate user by id
+	 * @param id user id
+	 * @returns user data with active: false
+	 */
+	deactvateUser(id: number) {
+		return this.httpClient.post<User>(
+			`${this.routeAceessName.userDeactivateURL(id)}`
+		);
+	}
+
+	/**
+	 * Activate user by id.
+	 * @param id user id
+	 * @returns user data with active: true
+	 */
+	activateUser(id: number) {
+		return this.httpClient.post<User>(
+			`${this.routeAceessName.userActivateURL(id)}`
+		);
+	}
+
+	/**
+	 * Update user roles by id.
+	 * @param id user id
+	 * @returns user data with new roles
+	 */
+	updateRoles(id: number, roles: UserRole[]) {
+		return this.httpClient.patch<User>(
+			`${this.routeAceessName.userUpdateRolesURL(id)}`,
+			{ roles }
+		);
+	}
+
+	/**
+	 * Update user position by id
+	 * @param id user id
+	 * @param position user position
+	 * @returns user data with new position
+	 */
+	updatePosition(id: number, position: updatePosition) {
+		return this.httpClient.patch<User>(
+			`${this.routeAceessName.userUpdatePositionURL(id)}`,
+			{
+				position,
+			}
+		);
+	}
+
+	/**
+	 * Update user password by id
+	 * @param id user id
+	 * @param password user password
+	 * @returns
+	 */
+	updatePassword(id: number, password: string) {
+		return this.httpClient.patch<User>(
+			`${this.routeAceessName.userUpdatePasswordURL(id)}`,
+			{
+				password,
+			}
+		);
+	}
+
+	/**
+	 * Get self profile
+	 * @returns user profile data
+	 */
+	getSelfProfile() {
+		return this.httpClient.get<User>(
+			`${this.routeAceessName.getSelfProfileURL()}`
+		);
+	}
+
+	/**
+	 * Update self password.
+	 * @param oldPassword user old password
+	 * @param newPassword user new password
+	 * @returns
+	 */
+	updateSelfPassword(
+		password: string,
+		oldPassword: string,
+		newPassword: string
+	) {
+		return this.httpClient.patch<User>(
+			`${this.routeAceessName.updateSelfPasswordURL()}`,
+			{
+				oldPassword,
+				newPassword,
+				password,
+			}
+		);
+	}
+
+	/**
+	 * Reset password
+	 * @param email user email
+	 * @returns
+	 */
+	forgotPassword(email: string) {
+		return this.httpClient.post<User>(
+			`${this.routeAceessName.forgotPasswordURL()}`,
+			{
+				email,
+			}
+		);
+	}
+
+	/**
+	 * Reset password.
+	 * @param email user email
+	 * @param password user password
+	 * @param token auth token
+	 * @returns
+	 */
+	resetPassword(email: string, password: string, token: string) {
+		return this.httpClient.post<User>(
+			`${this.routeAceessName.resetPasswordURL()}`,
+			{
+				email,
+				password,
+				token,
+			}
+		);
+	}
+
+	/**
+	 * Search
+	 * @param email user email
+	 * @param firstName user firstName
+	 * @param lastName user lastName
+	 * @returns
+	 */
+	search(email: string, firstName: string, lastName: string) {
+		return this.httpClient.get<User>(
+			`${this.routeAceessName.searchURL(email, firstName, lastName)}/`
+		);
 	}
 }
 
 export enum UserRole {
-	OWNER = 'OWNER',
-	ADMIN = 'ADMIN',
-	USER = 'USER',
+	OWNER = "OWNER",
+	ADMIN = "ADMIN",
+	USER = "USER",
 }
 /**
  * User entity
@@ -56,11 +274,11 @@ export interface User {
 	aboutMyself: string;
 	phone: {
 		id: string;
-		type: 'WORK' | 'MOBILE' | 'HOME';
+		type: "WORK" | "MOBILE" | "HOME";
 		value: string;
 	}[];
 	active: boolean;
-	birthday: string;
+	birthday: string | number;
 	timestamp: number;
 	showBirthYear: boolean;
 	roles: UserRole[];
@@ -71,3 +289,6 @@ export interface User {
 		link: string;
 	}[];
 }
+
+type updateUser = Omit<User, "id" | "active" | "position" | "roles">;
+type updatePosition = Pick<User, "position">;
